dda9aef1077c74c77ed2e2cd96dba33442dd6ef7
kazaimi <kazaimi@users.noreply.github.com>
2026-06-02 03:00:22 +0800

    chore: update cpolar backend URL [auto] 2026-06-02 03:00

--- src/store/gameState.js
+++ src/store/gameState.js
@@ -196,7 +196,7 @@ export const useGameStore = create((set, get) => ({
       const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
       const serverUrl = isLocal
         ? `http://${window.location.hostname}:3000`
-        : 'https://4a2dd55f.r16.cpolar.top';
+        : 'https://93c2b2b.r16.cpolar.top';
       socket = io(serverUrl, { transports: ['polling', 'websocket'] });
       socket.on('connect', () => {
         set({ socketConnected: true });
